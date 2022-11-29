7917a9a2222293f5b5dc093260d0a600157803f0
BT_4N2_13 <hsun7920@bths.edu>
2022-11-29 12:33:48 -0500

    the student class

--- src/Student.java
+++ src/Student.java
@@ -8,10 +8,25 @@ public class Student{
         this.gradeLevel = gradeLevel;
         this.school = school;
     }
+    public Student(String name, int gradeLevel){
+        this.name = name;
+        this.gradeLevel = gradeLevel;
+        if(gradeLevel > 8 && gradeLevel < 13){
+            school = "high school";
+        }
+
+        if(gradeLevel > 5 && gradeLevel < 9){
+            school = "middle school";
+        }
+
+        else {
+            school = "elementary school";
+        }
+    }
 
     public String toString()
     {
-        /* implement me, as described above */
+        return name + " is in " + gradeLevel + " and goes to " + school;
     }
 
 }
